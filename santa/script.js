b742461a8ce5a11529cd4f11583695112b82af8d
santa <santanaruse@gmail.com>
2023-06-07 03:05:45 +0900

    update: key-visual以外のmodel-viewerのdom操作を行うように変更

--- santa/script.js
+++ santa/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', (event) => {
   let modelViewer = document.querySelectorAll('model-viewer');
   let width = modelViewer[0].offsetWidth;
 
-  for (let i = 0; i < modelViewer.length; i++) {
+  for (let i = 1; i < modelViewer.length; i++ - 1) {
     modelViewer[i].style.height = width + 'px';
   }
 });
