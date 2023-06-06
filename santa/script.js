e907223b7ed16a493edbdf6b070d70a7a38bd5f1
santa <santanaruse@gmail.com>
2023-06-07 04:10:48 +0900

    key visualの型を構築

--- santa/script.js
+++ santa/script.js
@@ -1,17 +1,23 @@
 window.addEventListener('load', (event) => {
   let modelViewer = document.querySelectorAll('model-viewer');
-  let width = modelViewer[0].offsetWidth;
+  let width = modelViewer[1].offsetWidth;
+  let height = window.innerHeight;
 
   for (let i = 1; i < modelViewer.length; i++ - 1) {
     modelViewer[i].style.height = width + 'px';
   }
+  modelViewer[0].style.height = height + 'px';
 });
 
 window.addEventListener("resize", function () {
+
   let modelViewer = document.querySelectorAll('model-viewer');
-  let width = modelViewer[0].offsetWidth;
+  let width = modelViewer[1].offsetWidth;
+  let height = window.innerHeight;
 
   for (let i = 0; i < modelViewer.length; i++) {
     modelViewer[i].style.height = width + 'px';
+
+    modelViewer[0].style.height = height + 'px';
   }
 });
